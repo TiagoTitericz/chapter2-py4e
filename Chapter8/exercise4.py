c4e319ea985325a600add0045d1e44591a313b9b
TiagoTitericz <tiagotitericz@gmail.com>
2021-03-01 23:45:11 -0300

    Added unique words in a file exercise

---
+++ Chapter8/exercise4.py
@@ -0,0 +1,59 @@
+'''Exercise 4: Find all unique words in a file
+
+Shakespeare used over 20,000 words in his works. But how would you
+determine that? How would you produce the list of all the words that
+Shakespeare used? Would you download all his work, read it and track
+all unique words by hand?
+
+Let’s use Python to achieve that instead. List all unique words, sorted
+in alphabetical order, that are stored in a file romeo.txt containing a
+subset of Shakespeare’s work.
+
+To get started, download a copy of the file www.py4e.com/code3/romeo.txt.
+Create a list of unique words, which will contain the final result. Write
+a program to open the file romeo.txt and read it line by line. For each
+line, split the line into a list of words using the split function. For
+each word, check to see if the word is already in the list of unique
+words. If the word is not in the list of unique words, add it to the list.
+
+When the program completes, sort and print the list of unique words
+in alphabetical order.
+
+Enter file: romeo.txt
+['Arise', 'But', 'It', 'Juliet', 'Who', 'already',
+'and', 'breaks', 'east', 'envious', 'fair', 'grief',
+'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft',
+'sun', 'the', 'through', 'what', 'window',
+'with', 'yonder']'''
+
+fname = input("Enter file: ")
+sfile = open(fname)
+unique_words = list()
+for line in sfile:
+    words = line.split()
+    for word in words:
+        word = word.upper()
+        if word in unique_words:
+            continue
+        else:
+            unique_words.append(word)
+unique_words.sort()
+print(unique_words)
+
+'''
+fname = input("Enter file: ")
+sfile = open(fname)
+unique_words = list()
+for line in sfile:
+    words = line.split()
+    if unique_words == []:
+        unique_words.append(words[0])
+    for x in range(len(unique_words) - 1):
+        print(x)
+        for i in range(len(words) - 1):
+            print(x, i)
+            if unique_words[x] != words[i]:
+                unique_words.append(words[i])
+unique_words.sort()
+print(unique_words)'''
+    
