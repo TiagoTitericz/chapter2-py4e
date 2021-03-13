262e0f5bb6b60515046d4d46efa3ac35b1901596
TiagoTitericz <tiagotitericz@gmail.com>
2021-03-12 22:47:22 -0300

    Exercise 3 Chapter 10

--- Chapter10/exercise3.py
+++ Chapter10/exercise3.py
@@ -5,3 +5,29 @@ not count spaces, digits, punctuation, or anything other than the letters
 a-z. Find text samples from several different languages and see how
 letter frequency varies between languages. Compare your results with
 the tables at https://wikipedia.org/wiki/Letter_frequencies.'''
+
+import string
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+counts = dict()
+for line in fhand:
+    line = line.translate(str.maketrans('', '', string.punctuation))
+    line = line.lower()
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+lst = list()
+for key, val in counts.items():
+    lst.append((val, key))
+
+lst.sort(reverse=True)
+
+for key, val in lst:
+    print(val, key)
